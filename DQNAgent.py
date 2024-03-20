6594cd784c1e334727d1566bc189589d3d30c65f
Amelia Kovacs <ajk296@cornell.edu>
2024-03-20 13:22:32 -0400

    Updated test_agent.py for evaluating agents. Made file for each agent to standardize format

---
+++ DQNAgent.py
@@ -0,0 +1,11 @@
+import gymnasium as gym
+
+class DQNAgent:
+    def __init__(self, env):
+        self.env = env
+    
+    def train():
+        pass
+
+    def predict(self, observation):
+        pass
