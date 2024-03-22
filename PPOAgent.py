2cb1ec8dd9dac3d1d7e310459f621c02347d58a3
Max Pace <maxpace1@icloud.com>
2024-03-22 16:56:09 -0400

    Ambitious PPO with straightaways

--- PPOAgent.py
+++ PPOAgent.py
@@ -1,11 +1,63 @@
 import gymnasium as gym
 
+from stable_baselines3 import PPO
+
+from stable_baselines3.common.env_util import make_vec_env
+
+from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+
 class PPOAgent:
-    def __init__(self, env):
+    def __init__(self, env, model=None):
         self.env = env
-    
-    def train():
-        pass
+
+        if not model:
+            self.model = PPO(
+                "CnnPolicy",
+                env,
+                verbose=1,
+                learning_rate=0.0003,
+                n_steps=2048,
+                batch_size=64,
+                n_epochs=20,
+                gamma=0.99,
+            )
+        else:
+            self.model = PPO.load(model)
+
+    def train(self):
+        checkpoint_callback = CheckpointCallback(
+            save_freq=10000, save_path="./models/", name_prefix="ppo_car_racing"
+        )
+        self.model.learn(total_timesteps=100000, callback=checkpoint_callback)
+        self.model.save("ppo_car_racing")
 
     def predict(self, observation):
         pass
+
+
+def make_env(render_mode="rgb_array"):
+    env = gym.make(
+        "CarRacing-v2",
+        render_mode=render_mode,
+        lap_complete_percent=0.95,
+        domain_randomize=True,
+        continuous=False,
+    )
+    return env
+
+
+# env = make_vec_env(env_id=make_env, n_envs=4)
+env = DummyVecEnv([lambda: make_env(render_mode="human")])
+env = VecFrameStack(env, n_stack=4)
+agent = PPOAgent(env, model="models/ppo_car_racing_80000_steps")
+observation = env.reset()
+# agent.train()
+while True:
+    action, _states = agent.model.predict(observation)
+    observation, reward, done, info = env.step(action)
+    env.render()
+
+    if done.any():
+        observation = env.reset()
