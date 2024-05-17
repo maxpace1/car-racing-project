553c1277f1f73d4c3db85097fb08ab8e82957452
Amelia Kovacs <ajk296@cornell.edu>
2024-05-16 22:40:37 -0400

    new reward + visualizations

--- PPOAgent.py
+++ PPOAgent.py
@@ -7,8 +7,6 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 from stable_baselines3.common.callbacks import CheckpointCallback
 
-from rewardFunction import computeLosses
-
 
 class PPOAgent:
     def __init__(self, env, model=None):
@@ -30,59 +28,36 @@ class PPOAgent:
 
     def train(self):
         checkpoint_callback = CheckpointCallback(
-            save_freq=2500, save_path="./models/", name_prefix="ppo_car_racing_eco"
+            save_freq=10000, save_path="./models/", name_prefix="ppo_car_racing"
         )
-        self.model.learn(total_timesteps=1000000, callback=checkpoint_callback)
-        self.model.save("ppo_car_racing_final")
+        self.model.learn(total_timesteps=100000, callback=checkpoint_callback)
+        self.model.save("ppo_car_racing")
+
+    def predict(self, observation):
+        pass
 
 
-def make_env(render_mode="rgb_array", custom_reward=None):
+def make_env(render_mode="rgb_array"):
     env = gym.make(
         "CarRacing-v2",
         render_mode=render_mode,
-        lap_complete_percent=0.6,
+        lap_complete_percent=0.95,
         domain_randomize=True,
         continuous=False,
     )
-
-    original_step = env.step
-
-    #
-    def combine_rewards(reg_r, custom_r):
-        return (custom_r < 0) * reg_r + (custom_r >= 0) * reg_r * custom_r
-
-    def new_step(action):
-        out = original_step(action)
-        if len(out) == 4:
-            observation, reward, done, info = out
-            if custom_reward is not None:
-                reward = combine_rewards(reward, 1 - custom_reward(action, observation))
-            return observation, reward, done, info
-        elif len(out) == 5:
-            observation, reward, done, info, _ = out
-            if custom_reward is not None:
-                reward = combine_rewards(reward, 1 - custom_reward(action, observation))
-            return observation, reward, done, info, _
-        else:
-            return out
-
-    env.step = new_step
     return env
 
 
-# env = make_vec_env(env_id=lambda: make_env(custom_reward=computeLosses), n_envs=4)
-env = DummyVecEnv([lambda: make_env(render_mode="human", custom_reward=computeLosses)])
-env = VecFrameStack(env, n_stack=6)
-agent = PPOAgent(env, model="ppo_car_racing_480000_steps")
+# env = make_vec_env(env_id=make_env, n_envs=4)
+env = DummyVecEnv([lambda: make_env(render_mode="human")])
+env = VecFrameStack(env, n_stack=4)
+agent = PPOAgent(env, model="models/ppo_car_racing_80000_steps")
 observation = env.reset()
 # agent.train()
-tiles = 0
 while True:
     action, _states = agent.model.predict(observation)
     observation, reward, done, info = env.step(action)
-    if reward > 0:
-        tiles += 1
-        print(f"TILES: {tiles}")
     env.render()
+
     if done.any():
         observation = env.reset()
